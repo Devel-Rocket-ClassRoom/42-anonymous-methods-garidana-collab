ca9a71d502ed87194f00a22bfa2d4581a465d240
garidana-collab <garidana@gmail.com>
2026-03-10 21:19:24 +0900

    42
    Coding Practice 3 Complete

--- CodingPractice-03/Program.cs
+++ CodingPractice-03/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO.Pipes;
+using System.Security.Cryptography.X509Certificates;
 using static System.Console;
 
 
@@ -27,12 +29,54 @@ WriteLine($"람다식 (표현식): {lambda2(5)}");
 WriteLine();
 
 // 2
+// 익명 메서드는 매개변수를 생략 가능하다
 EventHandler handler1 = delegate { WriteLine("이벤트 처리됨."); };
-EventHandler handler2 = delegate (object obj, EventArgs events) { WriteLine("이벤트 처리됨"); };
+// 람다식의 경우에는 사용하지 않더라도 매개변수 명시가 필수임
+EventHandler handler2 = delegate (object obj, EventArgs events) { WriteLine("이벤트 처리됨."); };
 
 handler1(null, EventArgs.Empty);
 handler2(null, EventArgs.Empty);
 
+WriteLine();
+
+// 3
+// 빈 핸들러로 초기화
+GameEvent onScoreChange = delegate { }; // null 체크 불필요
+GameEvent onGameOver = delegate { }; // ''
+onScoreChange("점수 변경", 100);
+onGameOver("게임 종료", 0);
+
+// += 연산자로 delegate 핸들러를 추가 (delegate를 할당할 때와 똑같은 방식)
+onScoreChange += delegate (string eventname, int value)
+{
+    WriteLine($"[이벤트] {eventname}: {value}");
+};
+onScoreChange("점수 변경", 500);
+
+WriteLine();
+
+// 4
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+int sum = 0;
+
+// ProcessData 라는 함수를 정의
+// int 배열 data와 int형 매개변수를 받는 callback Action 델리게이트를 매개변수로 받는 함수임
+static void ProcessData(int[] data, Action<int> callback)
+{
+    foreach (int item in data)
+    {
+        callback(item);
+    }
+}
+
+// ProcessData 함수 호출
+// numbers 와 delegate (int n) 을 매개변수로 받음
+ProcessData(numbers, delegate (int n)
+{
+    sum += n;
+    WriteLine($"처리 중: {n}, 누적: {sum}");
+}); // 여기까지가 함수 호출한거임 
 
 
 
@@ -44,6 +88,7 @@ handler2(null, EventArgs.Empty);
 
 
 
+delegate void GameEvent(string eventName, int value);
 delegate void EventHandler(object obj, EventArgs events);
 delegate int Calculator(int i);
 
